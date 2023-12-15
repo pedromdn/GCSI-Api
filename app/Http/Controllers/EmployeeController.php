b458f739d15f67a9a27d85958ef14f6d464ebbc8
pedrolmdn <pedrolmdn@gmail.com>
2023-12-14 16:39:09 -0800

    images removed & public folder added to git ignore

--- app/Http/Controllers/EmployeeController.php
+++ app/Http/Controllers/EmployeeController.php
@@ -1,8 +1,6 @@
 <?php
 
 namespace App\Http\Controllers;
-use Illuminate\Support\Facades\Storage;
-use Illuminate\Support\Facades\Log;
 
 use App\Models\Employee;
 use Illuminate\Http\Request;
@@ -17,97 +15,90 @@ class EmployeeController extends Controller
 
     public function show($id)
     {
-        $employee = Employee::findOrFail($id); 
+        $employee = Employee::findOrFail($id);
 
         if ($employee->photo === null) {
             $employee->makeHidden('photo');
         } else {
             $employee->photo = url($employee->photo);
-        } 
+        }
 
         return response()->json(['employee' => $employee]);
     }
 
     public function store(Request $request)
-{
-    Log::info('store Request Data2:', $request->all());
+    {
 
-    $request->validate([
-        'name' => 'required|max:50',
-        'last_name1' => 'required|max:50',
-        'last_name2' => 'required|max:50',
-        'company' => 'required|max:100',
-        'area' => 'required|max:100',
-        'department' => 'required|max:100',
-        'position' => 'required|max:100',
-        'photo' => 'nullable',
-        'status' => 'required|boolean',
-    ]);
+        $request->validate([
+            'name' => 'required|max:50',
+            'last_name1' => 'required|max:50',
+            'last_name2' => 'required|max:50',
+            'company' => 'required|max:100',
+            'area' => 'required|max:100',
+            'department' => 'required|max:100',
+            'position' => 'required|max:100',
+            'photo' => 'nullable',
+            'status' => 'required|boolean',
+        ]);
 
-    $data = $request->all();
-    $currentDate = date('Y-m-d');
-    $data['startDate'] = $currentDate;
+        $data = $request->all();
+        $currentDate = date('Y-m-d');
+        $data['startDate'] = $currentDate;
 
-    if ($request->hasFile('photo')) {
+        if ($request->hasFile('photo')) {
+            $image = $request->file('photo');
+            $imageName = 'collaborator_' . time() . '.' . $image->getClientOriginalExtension();
+            $image->move(public_path('uploads/collaborator_images'), $imageName);
+            $data['photo'] = 'uploads/collaborator_images/' . $imageName;
+        }
 
-        $image = $request->file('photo');
-        $imageName = 'collaborator_' . time() . '.' . $image->getClientOriginalExtension();
-        $image->move(public_path('uploads/collaborator_images'), $imageName);
-        $data['photo'] = 'uploads/collaborator_images/' . $imageName;
-    }  
+        $employee = Employee::create($data);
 
-    $employee = Employee::create($data);
-
-    return response()->json(['employee' => $employee], 201);
-}
-
-public function update(Request $request, $id)
-{
-    Log::info('Update Request Data:', $request->all(), $id);
-    $employee = Employee::findOrFail($id);
-
-    $request->validate([
-        'name' => 'required|max:50',
-        'last_name1' => 'required|max:50',
-        'last_name2' => 'required|max:50',
-        'company' => 'required|max:100',
-        'area' => 'required|max:100',
-        'department' => 'required|max:100',
-        'position' => 'required|max:100',
-        'photo' => 'nullable|image|mimes:jpeg,png,jpg,gif|max:2048',
-        'status' => 'required|boolean',
-    ]);
-
-    $data = $request->except('startDate');
-
-    if ($request->hasFile('photo')) {       
-
-        $image = $request->file('photo');
-        $imageName = 'collaborator_' . time() . '.' . $image->getClientOriginalExtension();
-        $image->move('uploads/collaborator_images', $imageName);
-        $data['photo'] = 'uploads/collaborator_images/' . $imageName;
+        return response()->json(['employee' => $employee], 201);
     }
 
-    $employee->update($data);
+    public function update(Request $request, $id)
+    {
+        $employee = Employee::findOrFail($id);
 
-    return response()->json(['employee' => $employee]);
-}
+        $request->validate([
+            'name' => 'required|max:50',
+            'last_name1' => 'required|max:50',
+            'last_name2' => 'required|max:50',
+            'company' => 'required|max:100',
+            'area' => 'required|max:100',
+            'department' => 'required|max:100',
+            'position' => 'required|max:100',
+            'photo' => 'nullable|image|mimes:jpeg,png,jpg,gif|max:2048',
+            'status' => 'required|boolean',
+        ]);
 
+        $data = $request->except('startDate');
+
+        if ($request->hasFile('photo')) {
+            $image = $request->file('photo');
+            $imageName = 'collaborator_' . time() . '.' . $image->getClientOriginalExtension();
+            $image->move('uploads/collaborator_images', $imageName);
+            $data['photo'] = 'uploads/collaborator_images/' . $imageName;
+        }
+
+        $employee->update($data);
+
+        return response()->json(['employee' => $employee]);
+    }
 
     public function destroy($id)
     {
         try {
             $employee = Employee::findOrFail($id); // Encuentra el modelo Employee por su ID
-    
+
             $deletedEmployee = $employee->toArray(); // Guarda la información antes de eliminar
-    
+
             $employee->delete();
-    
+
             return response()->json(['employee' => $deletedEmployee], 200);
         } catch (\Exception $e) {
             return response()->json(['message' => 'Error al eliminar el colaborador', 'status' => 'error'], 500);
         }
     }
-    
-
 }
